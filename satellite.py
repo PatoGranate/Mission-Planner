Fixed quaternions pointing in incorrect directions bug

satellite.py
@@ -220,9 +220,9 @@ class Satellite:
         if self._cache_quats is None:
             for i in range(len(times)):
                 # Calculate the unit vector nadir pointing at t = 0
-                r0 = Vector3D(float(poses[i,0]),
-                              float(poses[i,1]),
-                              float(poses[i,2]))
+                r0 = Vector3D(-float(poses[i,0]),
+                              -float(poses[i,1]),
+                              -float(poses[i,2]))
                 r0_mag = r0.getNorm()
                 b3_unit = r0.scalarMultiply(1/r0_mag)
                 
@@ -239,7 +239,7 @@ class Satellite:
                 # Store vectors in direction cosine matrix from eci to body
                 DCM_b2e = np.array([(b1_unit.x, b1_unit.y, b1_unit.z), 
                                     (b2_unit.x, b2_unit.y, b2_unit.z), 
-                                    (b3_unit.x, b3_unit.y, b3_unit.z)])
+                                    (b3_unit.x, b3_unit.y, b3_unit.z)]).T
                 
                 quat = R.from_matrix(DCM_b2e).as_quat()
                 quats[i] = quat

visualization.py
@@ -5,23 +5,23 @@ from scipy.spatial.transform import Rotation as R
 import satellite_utils
 
 def plot_ground_tracks(sat_names, times):
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot()
+    fig = plt.figure()
+    ax = fig.add_subplot()
     earth_map = plt.imread('earth_outline.png')
-    ax1.imshow(earth_map, extent=[-180, 180, -90, 90], origin='upper', aspect='equal')
+    ax.imshow(earth_map, extent=[-180, 180, -90, 90], origin='upper', aspect='equal')
     
-    ax1.set_xlim([-180, 180])
-    ax1.set_ylim([-90, 90])
-    ax1.set_xticks(np.arange(-180, 181, 60.0))
-    ax1.set_yticks(np.arange(-90, 91, 30))
-    ax1.set_xlabel('Longitude (°)')
-    ax1.set_ylabel('Latitude (°)')
+    ax.set_xlim([-180, 180])
+    ax.set_ylim([-90, 90])
+    ax.set_xticks(np.arange(-180, 181, 60.0))
+    ax.set_yticks(np.arange(-90, 91, 30))
+    ax.set_xlabel('Longitude (°)')
+    ax.set_ylabel('Latitude (°)')
     
     for i in range(len(sat_names)):
         lonlat = sat_names[i].get_gtc(times)
-        ax1.plot(lonlat[:,0], lonlat[:,1])
+        ax.plot(lonlat[:,0], lonlat[:,1])
     
-    return fig1
+    return fig, ax
     
 def plot_orbits(sat_names, times):
     R_earth = 6.378e6
@@ -36,14 +36,14 @@ def plot_orbits(sat_names, times):
     Y = R_earth * np.sin(lat_grid) * np.sin(lon_grid)
     Z = R_earth * np.cos(lat_grid)
 
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(projection='3d')
-    ax2.plot_wireframe(X, Y, Z, color='black', alpha=0.5, linewidth=0.5)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_wireframe(X, Y, Z, color='black', alpha=0.5, linewidth=0.5)
     
     max_extent = 0
     for i in range(len(sat_names)):
         trajectory = sat_names[i].propagate(times)
-        ax2.plot(trajectory[:,0], trajectory[:,1], trajectory[:,2], 
+        ax.plot(trajectory[:,0], trajectory[:,1], trajectory[:,2], 
                  linewidth=2, label = sat_names[i].label)
         
         current_max = max(abs(trajectory[:,0]).max(), 
@@ -52,41 +52,48 @@ def plot_orbits(sat_names, times):
         if current_max > max_extent:
             max_extent = current_max
             
-    ax2.set_box_aspect([1, 1, 1])
+    ax.set_box_aspect([1, 1, 1])
     
-    ax2.set_xlim([-max_extent, max_extent])
-    ax2.set_ylim([-max_extent, max_extent])
-    ax2.set_zlim([-max_extent, max_extent])
-    ax2.legend()
-    ax2.set_xlabel('x-Pos (m)')
-    ax2.set_ylabel('y-Pos (m)')
-    ax2.set_zlabel('z-Pos (m)')
+    ax.set_xlim([-max_extent, max_extent])
+    ax.set_ylim([-max_extent, max_extent])
+    ax.set_zlim([-max_extent, max_extent])
+    ax.legend()
+    ax.set_xlabel('x-Pos (m)')
+    ax.set_ylabel('y-Pos (m)')
+    ax.set_zlabel('z-Pos (m)')
+    
+    return fig, ax
 
 def plot_cross_sat(satA, satB, times, tolerance):
     cross_dist = satellite_utils.get_cross_dist(satA, satB, times, tolerance)
 
-    fig3 = plt.figure()
-    ax3 = fig3.add_subplot(111)
-    ax3.plot(times, cross_dist)
-    ax3.axhline(y = tolerance, color = 'r', linestyle = '--')
-    ax3.set_xlabel('Time elapsed (s)')
-    ax3.set_ylabel('Cross-sat distance (m)')
-    ax3.set_xlim([0, max(times)+1])
-    ax3.set_ylim(bottom = 0)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(times, cross_dist)
+    ax.axhline(y = tolerance, color = 'r', linestyle = '--')
+    ax.set_xlabel('Time elapsed (s)')
+    ax.set_ylabel('Cross-sat distance (m)')
+    ax.set_xlim([0, max(times)+1])
+    ax.set_ylim(bottom = 0)
+    
+    return fig, ax
 
 
 
 
-def animate_sat_attitude(satA, times, interval = 100):
-    quats = satA.get_quats(times)
+def animate_sat_attitude(sat_names, times, interval = 100):
+    quats = sat_names[0].get_quats(times)
     rots = R.from_quat(quats)
     DCM_eci2body = rots.as_matrix()
     DCM_body2eci = DCM_eci2body.transpose(0, 2, 1)
     
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection = '3d')
-    ax.set_xlim(-1, 1), ax.set_ylim(-1, 1), ax.set_zlim(-1, 1)
-    ax.set_xlabel('X'), ax.set_ylabel('Y'), ax.set_zlabel('Z')
+    fig, ax = plot_orbits(sat_names, times)
+    
+    obs_traj = sat_names[0].propagate(times)
+    obs_marker = ax.scatter(obs_traj[0,0], obs_traj[0,1], obs_traj[0,2], 
+                            s = 50, label = sat_names[0].label)
+    
+    
     
     quiver = ax.quiver(
     [0,0,0], [0,0,0], [0,0,0],    
@@ -95,7 +102,12 @@ def animate_sat_attitude(satA, times, interval = 100):
     
     def update(i):
         nonlocal quiver
+        
+        obs_x, obs_y, obs_z = obs_traj[i]
+        obs_marker._offsets3d = ([obs_x], [obs_y], [obs_z])
+        
         quiver.remove()
+        
         
         axes_sat = DCM_body2eci[i]
         U = axes_sat[:,0]
@@ -103,11 +115,13 @@ def animate_sat_attitude(satA, times, interval = 100):
         W = axes_sat[:,2]
         
         quiver = ax.quiver(
-        [0,0,0], [0,0,0], [0,0,0],    
-        U, V, W, # initial directions = body-axes aligned with ECI
-        length=1)
+            [obs_x, obs_x, obs_x],
+            [obs_y, obs_y, obs_y],
+            [obs_z, obs_z, obs_z],   
+            U, V, W, length=1e6, normalize = True)
         
-        return quiver,
+        
+        return quiver, obs_marker,
     
     anim = FuncAnimation(fig, update, frames = len(times),
                          interval = interval, blit = False)